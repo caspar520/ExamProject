478efa4011df4aee2a85a7175854cf01949db80b
opensourceBrown <623367956@qq.com>
2013-07-24 08:26:43 +0800

    修改了简答题界面的ui，添加了键盘弹出、消失后页面的动画

--- EXShortAnswerTopicView.m
+++ EXShortAnswerTopicView.m
@@ -24,14 +24,21 @@
     return self;
 }
 
+- (void)dealloc{
+    [answerTextView resignFirstResponder];
+    [self keyboardWillDisapper];
+    [super dealloc];
+}
+
 - (void)refreshUI{
     [super refreshUI];
+    optionTipLabel.text=@"输入答案";
     if (answerTextView==nil) {
-        answerTextView=[[UITextView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(answerContainerView.frame), CGRectGetHeight(answerContainerView.frame))];
+        answerTextView=[[UITextView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(answerContainerView.frame)-20, CGRectGetHeight(answerContainerView.frame)-20)];
         answerTextView.delegate = self;
         answerTextView.returnKeyType = UIReturnKeyDone;
         answerTextView.keyboardType = UIKeyboardTypeDefault;
-        answerTextView.backgroundColor = [UIColor grayColor];
+        answerTextView.backgroundColor = [UIColor clearColor];
         answerTextView.font = [UIFont systemFontOfSize:20];
         answerTextView.alpha=1.0;
         answerTextView.editable=YES;
@@ -39,9 +46,33 @@
     }
 }
 
+#pragma mark keyBoard Event
+- (void)keyboardWillAppear {
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:0.3];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
+    self.center=CGPointMake(self.center.x, self.center.y-160);
+    [UIView commitAnimations];
+}
+
+-(void)keyboardWillDisapper {
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:0.3];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
+    self.center=CGPointMake(self.center.x, self.center.y+160);
+    [UIView commitAnimations];
+}
+
 #pragma mark UITextViewDelegate
+- (void)textViewDidBeginEditing:(UITextView *)textView{
+    [answerTextView becomeFirstResponder];
+    [self keyboardWillAppear];
+}
+
 - (void)textViewDidEndEditing:(UITextView *)textView{
     [self updateSelectedResult];
+    [answerTextView resignFirstResponder];
+    [self keyboardWillDisapper];
 }
 
 @end
