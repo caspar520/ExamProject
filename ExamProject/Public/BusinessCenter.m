af6d542b6c593cc23f9e629441cb545ae10c2df4
sgeniusd <magic@magictekiMac-mini.local>
2013-10-08 21:22:33 -0700

    注释读取keychain，先从userdefault中管理

--- ExamProject/Public/BusinessCenter.m
+++ ExamProject/Public/BusinessCenter.m
@@ -33,59 +33,59 @@ static BusinessCenter *instance = nil;
 
 - (BOOL)isLogin            //判断是否登录    从keychain中读取登录信息
 {
-#if TARGET_IPHONE_SIMULATOR
+//#if TARGET_IPHONE_SIMULATOR
     NSString *usr = [[NSUserDefaults standardUserDefaults]objectForKey:KEYCHAIN_USRNAME];
     NSString *pwd = [[NSUserDefaults standardUserDefaults]objectForKey:KEYCHAIN_PWD];
     return (usr != nil && pwd != nil);
-#else
-    return ([_keychainWrapper objectForKey:KEYCHAIN_USRNAME] != nil
-            && [_keychainWrapper objectForKey:KEYCHAIN_PWD] != nil);
-#endif
+//#else
+//    return ([_keychainWrapper objectForKey:KEYCHAIN_USRNAME] != nil
+//            && [_keychainWrapper objectForKey:KEYCHAIN_PWD] != nil);
+//#endif
 }
 
 //保存用户密码到keychain
 - (void)saveUsername:(NSString *)userName andPwd:(NSString *)password
 {
     if (userName && password) {
-#if TARGET_IPHONE_SIMULATOR
+//#if TARGET_IPHONE_SIMULATOR
         [[NSUserDefaults standardUserDefaults]setObject:userName forKey:KEYCHAIN_USRNAME];
         [[NSUserDefaults standardUserDefaults]setObject:password forKey:KEYCHAIN_PWD];
         [[NSUserDefaults standardUserDefaults]synchronize];
-#else
-        [_keychainWrapper setObject:userName forKey:KEYCHAIN_USRNAME];
-        [_keychainWrapper setObject:password forKey:KEYCHAIN_PWD];
-#endif
+//#else
+//        [_keychainWrapper setObject:userName forKey:KEYCHAIN_USRNAME];
+//        [_keychainWrapper setObject:password forKey:KEYCHAIN_PWD];
+//#endif
     }
 }
 
 - (id)getUserName
 {
-#if TARGET_IPHONE_SIMULATOR
+//#if TARGET_IPHONE_SIMULATOR
     return [[NSUserDefaults standardUserDefaults] objectForKey:KEYCHAIN_USRNAME];
-#else
-    return [_keychainWrapper objectForKey:KEYCHAIN_USRNAME];
-#endif
+//#else
+//    return [_keychainWrapper objectForKey:KEYCHAIN_USRNAME];
+//#endif
 }
 
 - (id)getUserPassword
 {
-#if TARGET_IPHONE_SIMULATOR
+//#if TARGET_IPHONE_SIMULATOR
     return [[NSUserDefaults standardUserDefaults] objectForKey:KEYCHAIN_PWD];
-#else
-    return [_keychainWrapper objectForKey:KEYCHAIN_PWD];
-#endif
+//#else
+//    return [_keychainWrapper objectForKey:KEYCHAIN_PWD];
+//#endif
 }
 
 - (void)deleteIdentifierInfoFormKeyChain
 {
-#if TARGET_IPHONE_SIMULATOR
+//#if TARGET_IPHONE_SIMULATOR
     [[NSUserDefaults standardUserDefaults]removeObjectForKey:KEYCHAIN_USRNAME];
     [[NSUserDefaults standardUserDefaults]removeObjectForKey:KEYCHAIN_PWD];
     [[NSUserDefaults standardUserDefaults]synchronize];
-#else
-    [_keychainWrapper setObject:nil forKey:KEYCHAIN_USRNAME];
-    [_keychainWrapper setObject:nil forKey:KEYCHAIN_PWD];
-#endif
+//#else
+//    [_keychainWrapper setObject:nil forKey:KEYCHAIN_USRNAME];
+//    [_keychainWrapper setObject:nil forKey:KEYCHAIN_PWD];
+//#endif
     
 }
 
